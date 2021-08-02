e52f5bf97bd866b8ee24e08d05336ffc64dd9181
over40dev <code@over40.dev>
2021-08-02 16:57:23 -0230

    Add HTML DateTime picker

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,22 +2,29 @@ import {useState} from 'react';
 
 export default function AddTask() {
 
+  const [text, setText] = useState('');
+  const [datetime, setDateTime] = useState(Date());
+  const [reminder, setReminder] = useState(false);
+
   return (
     <form className="add-form">
       <div className="form-control">
         <label htmlFor="taskText">Task</label>
         <input 
-          name="taskText" type="text" placeholder="Add Task" />
+          name="taskText" type="text" placeholder="Add Task" 
+          value={text} onChange={(e) => setText(e.target.value)} />
       </div>
       <div className="form-control">
         <label htmlFor="datetime">Day & Time</label>
         <input 
-          name="datetime" type="text" placeholder="Add Date and Time" />
+          name="datetime" type="datetime-local" placeholder="Add Date and Time" 
+          value={datetime} onChange={(e) => setDateTime(e.target.value)} />
       </div>
       <div className="form-control form-control-check">
         <label htmlFor="reminder">Set Reminder</label>
         <input 
-          name="reminder" type="checkbox" />
+          name="reminder" type="checkbox" 
+          value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
       </div>
       
       <input type="submit" value="Save Task" className="btn btn-block"/>
