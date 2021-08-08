56372dc13409be69bf5ae720006e9c9922b973be
over40dev <code@over40.dev>
2021-08-08 18:08:50 -0230

    React-Router  useLocation Hook in Header & Footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { useLocation } from 'react-router';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
 export default function Header({title, showForm, onToggleForm}) {
 
+  const locationHomePage = useLocation().pathname === '/';
+
   return (
     <header className="header">
       <h1>{title}</h1>
+      {locationHomePage && 
       <Button 
         text={showForm ? 'Close' : 'Add'}
         color={showForm ? 'red' : 'green'}
         handleClick={() => onToggleForm(!showForm)}
-        />
+        />}
     </header>
   )
 }
